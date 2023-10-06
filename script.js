a3f21afdd1f813f8414714e58b2169b1d066ffc1
QuitasolKenlei <146052692+QuitasolKenlei@users.noreply.github.com>
2023-10-06 11:47:34 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let firstFavNum = 89;
+let secondFavNum = 9;
+
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNum - secondFavNum);
+console.log(firstFavNum * secondFavNum);
+console.log(firstFavNum / secondFavNum);
+console.log(firstFavNum % secondFavNum);
+console.log(firstFavNum ** secondFavNum);
+
